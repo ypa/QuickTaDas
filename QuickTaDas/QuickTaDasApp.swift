45d92442b033a571d4fc0724dfcb36d0f22dcf9d
Yan P Aung <yan.pye.aung@gmail.com>
2021-12-25 19:34:24 -0800

    Initial Commit

---
+++ QuickTaDas/QuickTaDasApp.swift
@@ -0,0 +1,20 @@
+//
+//  QuickTaDasApp.swift
+//  QuickTaDas
+//
+//  Created by YAN AUNG on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct QuickTaDasApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
