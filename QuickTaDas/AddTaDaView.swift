5337f4262363fa63a1bc087d929a7df868d5ce23
Yan P Aung <yan.pye.aung@gmail.com>
2021-12-29 20:06:10 -0800

    Persist ToDo title to CoreData entity

--- QuickTaDas/AddTaDaView.swift
+++ QuickTaDas/AddTaDaView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct AddTaDaView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @State var toDoTitle = ""
+    
     var body: some View {
-        Text("Add View")
+        TextField("Buy Cheese", text: $toDoTitle)
+            .padding()
+            .toolbar {
+                Button(action: {
+                    
+                    let newToDo = ToDo(context: viewContext)
+                    newToDo.title = toDoTitle
+                    
+                    do {
+                        try viewContext.save()
+                    } catch {
+                        // Replace this implementation with code to handle the error appropriately.
+                        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                        let nsError = error as NSError
+                        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                    }
+                    
+                }) {
+                    Text("Add to List")
+                }
+            }
     }
 }
 
 struct AddTaDaView_Previews: PreviewProvider {
     static var previews: some View {
-        AddTaDaView()
+        NavigationView {
+            AddTaDaView()
+        }
     }
 }
