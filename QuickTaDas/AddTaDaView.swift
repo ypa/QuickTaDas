42baf8e0cfa35f96e828c79cec9c073d1b513134
Yan P Aung <yan.pye.aung@gmail.com>
2022-01-01 20:47:47 -0800

    Refactor to extract out addToDo as a function and other improvements

--- QuickTaDas/AddTaDaView.swift
+++ QuickTaDas/AddTaDaView.swift
@@ -12,31 +12,53 @@ struct AddTaDaView: View {
     @Environment(\.presentationMode) var presentationMode
     
     @State var toDoTitle = ""
+    @FocusState var titleFocus: Bool
     
     var body: some View {
-        TextField("Buy Cheese", text: $toDoTitle)
-            .padding()
-            .toolbar {
-                Button(action: {
-                    
-                    let newToDo = ToDo(context: viewContext)
-                    newToDo.title = toDoTitle
-                    
-                    do {
-                        try viewContext.save()
-                        // pop back up to the previous view
-                        presentationMode.wrappedValue.dismiss()
-                    } catch {
-                        // Replace this implementation with code to handle the error appropriately.
-                        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                        let nsError = error as NSError
-                        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-                    }
-                    
-                }) {
-                    Text("Add to List")
+        VStack {
+            TextField("Buy Cheese", text: $toDoTitle)
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+                .focused($titleFocus)
+                .submitLabel(.go)
+                .onSubmit {
+                    addToDo()
                 }
-            }
+                .padding()
+                .toolbar {
+                    Button(action: {
+                        addToDo()
+                    }) {
+                        Text("Add to List")
+                    }
+                }
+            Spacer()
+        }
+        // Make Keyboard to show up when title field is focus
+        .onAppear {
+            /* Workaround a bug with XCode older version : not needed anymore
+             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+             titleFocus = true
+             }
+             */
+            titleFocus = true
+        }
+    }
+
+    func addToDo() {
+        let newToDo = ToDo(context: viewContext)
+        newToDo.title = toDoTitle
+
+        do {
+            try viewContext.save()
+            // pop back up to the previous view
+            presentationMode.wrappedValue.dismiss()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
     }
 }
 
